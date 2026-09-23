9b08de6333959d8d3fae71a05ff28fb14c0f5fb9
thataxelguy <axel.woermann@gmail.com>
2026-09-23 16:43:10 +0200

    Implement SparseArray length

--- main.py
+++ main.py
@@ -10,13 +10,18 @@ class SparseArray():
 
     def __str__(self) -> str:
         new_list = []
-        for i in range(0, self._length):
+        for i in range(self._length):
             if i in self._data:
                 new_list.append(self._data[i])
             else:
                 new_list.append(0)
         return f"{new_list}"
 
+    def __len__(self) -> int:
+        return self._length
+
 
 sa = SparseArray([1, 0, 1, 2, 0, 100, 0, 1])
 print(sa)
+print(len(sa))
+print(type(sa))
