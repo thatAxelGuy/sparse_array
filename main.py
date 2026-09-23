8a12f067c97ed4c084bf40b0478bb9dce9d694b4
thataxelguy <axel.woermann@gmail.com>
2026-09-23 15:28:02 +0200

    Add SparseArray constructor and string representation

---
+++ main.py
@@ -0,0 +1,22 @@
+class SparseArray():
+
+    def __init__(self, values: list[int]) -> None:
+        self._length = len(values)
+        self._data = {}
+
+        for index, value in enumerate(values):
+            if value != 0:
+                self._data[index] = value
+
+    def __str__(self) -> str:
+        new_list = []
+        for i in range(0, self._length):
+            if i in self._data:
+                new_list.append(self._data[i])
+            else:
+                new_list.append(0)
+        return f"{new_list}"
+
+
+sa = SparseArray([1, 0, 1, 2, 0, 100, 0, 1])
+print(sa)
